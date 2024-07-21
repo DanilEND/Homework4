721cc12fb3684fa82c795069a1c5fa53e6390a54
DanilEND <enders.danil@mail.ru>
2024-07-21 21:20:52 +0700

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,10 @@
+my_string = input("Как вас зовут?: ")
+print("Добрый день,", my_string)
+print(len(my_string))
+
+my_string1 = ("Добрый день, Данил, как ваши дела?")
+print(my_string1.upper())
+print(my_string1.lower())
+print(my_string1.replace(" ", ""))
+print(my_string1 [0])
+print(my_string1 [-1])
